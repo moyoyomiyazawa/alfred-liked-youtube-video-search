9eaf8a858bc1b30cf7c04cc20f76dc915d734084
Yuya Kasai <moyoyomiyazawa@gmail.com>
2021-03-05 17:16:51 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const alfy = require('alfy');
+const { google } = require('googleapis');
+const OAuth2 = google.auth.OAuth2;
+const createItem = require('./lib/createItem');
+
+const getAuthorizedClient = require('./lib/getAuthorizedClient');
+const getFavoriteVideos = require('./lib/getFavoriteVideos');
+
+const scopes = ['https://www.googleapis.com/auth/youtube.readonly'];
+const tokenPath = __dirname + '/' + 'alfred-youtube-search.json';
+const authorizationCode = process.env.AUTHORIZATION_CODE;
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+
+const oauth2Client = new OAuth2(clientId, clientSecret, 'urn:ietf:wg:oauth:2.0:oob');
+
+
+(async () => {
+  const auth = await getAuthorizedClient(oauth2Client, tokenPath, authorizationCode, scopes);
+  const videos = await getFavoriteVideos(auth);
+
+  const items = alfy.inputMatches(videos, 'snippet.title').map((video) =>
+    createItem(
+      video.snippet.title,
+      video.snippet.channelTitle,
+      `https://www.youtube.com/watch?v=${video.id}`
+    )
+  );
+
+  alfy.output(items);
+
+  // TODO: インクリメンタルサーチを実装する（なぜかalfy.inputが取得できないので調査する）
+})();
